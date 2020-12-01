2d2cfe1dfcf5a7132a7d9e2d405e0b95977f0b37
Scott Carrington <scarrington76@gmail.com>
2020-12-01 16:58:23 -0700

    added initial todos file

---
+++ source/pythonclient/todos.py
@@ -0,0 +1,4 @@
+#todo pull SPY or SPX every single morning and sell every single close
+#todo create dataframe for trading, start naming the functional concepts for trade
+#todo create a trailing stop for each stock
+#todo create a ceiling stop from each stock
