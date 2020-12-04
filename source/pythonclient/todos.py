cad0326c84b9d6104f69ef129aa2bb417a88b844
Scott Carrington <scarrington76@gmail.com>
2020-12-04 13:01:06 -0700

    added todos

--- source/pythonclient/todos.py
+++ source/pythonclient/todos.py
@@ -2,3 +2,9 @@
 #todo create dataframe for trading, start naming the functional concepts for trade
 #todo create a trailing stop for each stock
 #todo create a ceiling stop from each stock
+#todo Maintain record keeping - buy and sell realized and unrealized gains, div payments and current/last div, annual yield
+#todo warning or some real time indicator of potential wash sales
+#todo 52 week high and low
+#todo research measurement of support and resistance lines
+#todo check the prices every x mins
+#todo stock screener - what params are available?
