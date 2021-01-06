1c3101aa9551eb2a98f57504877147673f2f9c7e
Scott Carrington <scarrington76@gmail.com>
2021-01-05 21:40:53 -0700

    Test functionality of historical candlebars in main

--- source/pythonclient/HelloWorld.py
+++ source/pythonclient/HelloWorld.py
@@ -1,10 +1,15 @@
+import threading
+import time
+
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 from ibapi.contract import  Contract
+import pandas
 
-class TestApp(EWrapper, EClient):
+class IBapi(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
+        self.data = []
 
     def error(self, reqId, errorCode, errorString):
         print('Error: ', reqId, " ", errorCode, " ", errorString)
@@ -12,22 +17,40 @@ class TestApp(EWrapper, EClient):
     def contractDetails(self, reqId:int, contractDetails):
         print('contractDetails: ', reqId, ' ', contractDetails)
 
-def main():
-    app = TestApp()
-    app.connect('127.0.0.1', 7497, 0)
+    def historicalData(self, reqId, bar):
+        print(f'Time: {bar.date} Close: {bar.close}')
+        self.data.append([bar.date, bar.close])
 
-    contract = Contract()
-    contract.symbol = 'AAPL'
-    contract.secType = 'STK'
-    contract.exchange = 'SMART'
-    contract.currency = 'USD'
-    contract.primaryExchange = 'NASDAQ'
+def run_loop():
+	app.run()
 
-    app.reqContractDetails(1, contract)
 
-    app.run()
+app = IBapi()
+app.connect('127.0.0.1', 7497, 0)
 
-if __name__ == "__main__":
-    main()
+# Start the socket in a thread
+api_thread = threading.Thread(target=run_loop, daemon=True)
+api_thread.start()
+
+time.sleep(1)  # Sleep interval to allow time for connection to server
+
+# Create contract object
+eurusd_contract = Contract()
+eurusd_contract.symbol = 'JD'
+eurusd_contract.secType = 'STK'
+eurusd_contract.exchange = 'ISLAND'
+eurusd_contract.currency = 'USD'
+
+# Request historical candles
+app.reqHistoricalData(1, eurusd_contract, '', '2 D', '1 hour', 'BID', 0, 2, False, [])
+
+time.sleep(5)  # sleep to allow enough time for data to be returned
+df = pandas.DataFrame(app.data, columns=['DateTime', 'Close'])
+df['DateTime'] = pandas.to_datetime(df['DateTime'], unit='s')
+df.to_csv('EURUSD_Hourly.csv')
+
+print(df)
+
+
 
 
